a76f3bd983bb6a1d1fb0e2a29fdf5c6192cf4d84
Shankar Panaskar <shankar.crezvatic@gmail.com>
2024-08-08 17:00:22 +0530

    reset 2 commit

---
+++ application/views/layout/footer.php
@@ -0,0 +1,386 @@
+<!-- ============================================================== -->
+<!-- Floating icons of social media  Section -->
+<!-- ============================================================= -->
+
+<div class="floating-social-icons d-flex flex-column gap-1 align-items-center bg-white p-2 justify-content-center mx-auto">
+	<a href="#"><i class="fab fa-facebook-f fs-3 navyText "></i></a>
+	<!-- <a href="#"><i class="fab fa-behance-square fs-3"></i></a> -->
+	<a href="#"><i class="fab fa-youtube fs-3 navyText "></i></a>
+	<a href="#"><i class="fab fa-linkedin-in fs-3 navyText "></i></a>
+	<a href="#"><i class="fab fa-instagram fs-3 navyText "></i></a>
+
+	<!-- <a href="#"><i class="fab fa-pinterest fs-3"></i></a> -->
+	<span></span>
+</div>
+
+
+<!-- ============================================================== -->
+<!-- Start - Brand- Section -->
+<!-- ============================================================= -->
+<div class="brand-section">
+	<div class="container">
+		<div class="row brand-bg">
+			<div class="brand_list owl-carousel">
+				<div class="brand-thumb">
+					<img src="assets/images/footer-client-logo/Dalmia_Logo.png" alt="">
+				</div>
+				<div class="brand-thumb">
+					<img src="assets/images/footer-client-logo/Jindal_Logo.png" alt="">
+				</div>
+				<div class="brand-thumb">
+					<img src="assets/images/footer-client-logo/JSWSteel_Logo.png" alt="">
+				</div>
+				<div class="brand-thumb">
+					<img src="assets/images/footer-client-logo/Lechler_Logo.png" alt="">
+				</div>
+				<div class="brand-thumb">
+					<img src="assets/images/footer-client-logo/Dalmia_Logo.png" alt="">
+				</div>
+				<div class="brand-thumb">
+					<img src="assets/images/footer-client-logo/Jindal_Logo.png" alt="">
+				</div>
+				<div class="brand-thumb">
+					<img src="assets/images/footer-client-logo/JSWSteel_Logo.png" alt="">
+				</div>
+				<div class="brand-thumb">
+					<img src="assets/images/footer-client-logo/Lechler_Logo.png" alt="">
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- ============================================================== -->
+<!-- Start - Footer- Section -->
+<!-- ============================================================= -->
+<div class="footer-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3 col-md-6">
+				<div class="footer-title">
+					<h2>About Us.</h2>
+				</div>
+				<div class="footer-text">
+					<p>We are experienced professionals who understand that It services is changing, and are true
+						partners who care about your future business success.We are experienced professionals who
+						understand that It services is changing,</p>
+				</div>
+				<div class="footer-address">
+					<div class="footer-social-icon">
+						<ul>
+							<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+							<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<div class="footer-title">
+					<h2>Useful Links</h2>
+				</div>
+				<div class="footer-menu">
+					<ul>
+						<li><a href="">About</a></li>
+						<li><a href="">News</a></li>
+						<li><a href="">Partners</a></li>
+						<li><a href="">Room Details</a></li>
+						<li><a href="">Gallery</a></li>
+						<li><a href="">Contacts</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<div class="footer-title">
+					<h2>Help?</h2>
+				</div>
+				<div class="footer-menu">
+					<ul>
+						<li><a href="">FAQ</a></li>
+						<li><a href="">Term & conditions</a></li>
+						<li><a href="">Reporting</a></li>
+						<li><a href="">Documentation</a></li>
+						<li><a href="">Suport Policy</a></li>
+						<li><a href="">Privacy</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<div class="footer-logo">
+					<img style="width: 100%;" src="assets/images/duztecLogo-PNG.PNG" alt="">
+					<!-- <img src="assets/images/fav-icon/transparent-logo.png" alt=""> -->
+				</div>
+				<div class="footer-right-text">
+					<p>We are experienced professionals who understand that It services is</p>
+				</div>
+				<div class="footer-right-side">
+					<span><i class="fas fa fa-map-marker"></i> 254 Lillian Blvd,Holbrook</span>
+				</div>
+				<div class="footer-right-side">
+					<span><i class="fas fa fa-phone"></i> 254 Lillian Blvd,Holbrook</span>
+				</div>
+				<div class="footer-right-side">
+					<span><i class="fas fa fa-envelope"></i> 254 Lillian Blvd,Holbrook</span>
+				</div>
+				<!-- <div class="footer-btn">
+					<form id="contact_form2" action="mail2.php" method="POST">
+						<input type="email" placeholder="Enter email address" class="footer-email" required>
+						<button type="submit" class="btn-control">Subscribe</button>
+					</form>
+				</div> -->
+			</div>
+		</div>
+	</div>
+</div>
+<!-- footer-copyright-section -->
+<div class="footer-bottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6">
+				<div class="copy-right-text">
+					<p>© Dream-It 2022 All rights reserved.</p>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6">
+				<div class="footer-menu-right">
+					<ul>
+						<li><a href="#">Terms of Use</a></li>
+						<li><a class="br-none" href="#">Privacy Policy</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--==================================================-->
+<!-- Start Search Popup Area -->
+<!--==================================================-->
+<div class="search-popup">
+	<button class="close-search style-two"><i class="fas fa-times"></i></button>
+	<button class="close-search"><i class="fas fa-arrow-up"></i></button>
+	<form method="post" action="#">
+		<div class="form-group">
+			<input type="search" name="search-field" value="" placeholder="Search Here" required="">
+			<button type="submit"><i class="fas fa-search"></i></button>
+		</div>
+	</form>
+</div>
+<!--==================================================-->
+<!-- End Search Popup Area -->
+<!--==================================================-->
+<!-- gallery js -->
+<!-- Load jQuery -->
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> -->
+<!-- Load jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+	integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+	crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+	integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+	crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- Add other scripts as necessary -->
+<script src="assets/js/gallery.js"></script>
+<script src="assets/js/sectors.js"></script>
+<script src="assets/js/prod_detail.js"></script>
+
+
+<!-- bootstrap js -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- particles Js -->
+<script src="assets/js/particles.min.js"></script>
+<script src="assets/js/stats.js"></script>
+<script src="assets/js/jquery.appear.js"></script>
+<!-- Ajax Mail -->
+<script src="assets/js/ajax-mail.js"></script>
+<!-- carousel js -->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!-- counterup js -->
+<script src="assets/js/jquery.counterup.min.js"></script>
+<!-- Swiper JS -->
+<!-- waypoints js -->
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<!-- Testimonial js -->
+<!-- imagesloaded js -->
+<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+<!-- isotope js -->
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<!-- animated js -->
+<script src="assets/js/animated-text.js"></script>
+<!-- Wow Js -->
+<script src="assets/js/wow.min.js"></script>
+<!-- venobox js -->
+<script src="venobox/venobox.js"></script>
+<!-- venobox min js -->
+<script src="venobox/venobox.min.js"></script>
+<!-- jquery js -->
+<script src="assets/js/jquery.meanmenu.js"></script>
+<script src="assets/js/jquery.nav.js"></script>
+<script src="assets/js/jquery.scrollUp.js"></script>
+<!-- theme js -->
+<script src="assets/js/theme.js"></script>
+<script>
+	let index = 0;
+	const slides = document.querySelectorAll('.slide');
+	const slider = document.querySelector('.slider');
+
+	document.querySelector('.prev').addEventListener('click', () => {
+		index = (index > 0) ? index - 1 : slides.length - 1;
+		updateSlider();
+	});
+
+	document.querySelector('.next').addEventListener('click', () => {
+		index = (index < slides.length - 1) ? index + 1 : 0;
+		updateSlider();
+	});
+
+	function updateSlider() {
+		slider.style.transform = `translateX(${-index * 100}%)`;
+	}
+</script>
+
+
+
+<!-- particles.js lib - https://github.com/VincentGarreau/particles.js -->
+<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<!-- stats.js lib -->
+<script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
+
+<script>
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "image",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "./assets/images/fevicon.png",
+                "width": 10,
+                "height": 15
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            // "value": 32,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 300,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                // "size": 20,
+				"size": 50,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+    stats.begin();
+    stats.end();
+    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+    requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+</script>
+
+</body>
+
+</html>
