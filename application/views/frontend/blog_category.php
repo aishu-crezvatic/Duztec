5316f089b8b2fdda692b40aafefc25a9b4d54f5b
aishu-crezvatic <aishwarya@crezvatic.com>
2024-09-03 12:53:22 +0530

    pull client chnages done by siddhesh

--- application/views/frontend/blog_category.php
+++ application/views/frontend/blog_category.php
@@ -10,84 +10,55 @@
 <!-- Start - Slider- Section -->
 <!-- ============================================================= -->
 <div class="breadcumn-section-blog d-flex align-items-center">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12 col-md-12">
-				<!-- <div class="breadcumn-content text-left" class="wow slideInLeft" data-wow-duration="2s" data-wow-delay=".5s">
-						<h2>All Blogs</h2>
-						<ul>
-							<li><a href="index.html"> <i class="fas fa-home"></i> </a></li>
-							<li class="style2">Page</li>
-							<li><i class="fas fa-chevron-right"></i></li>
-							<li class="style2">All Blogs</li>
-						</ul>
-					</div> -->
-			</div>
-		</div>
-	</div>
-<<<<<<< HEAD
-	<!-- ============================================================== -->
-	<!-- Start - Service- Section -->
-	<!-- ============================================================= -->
-	<div class="portfolio-section single-style">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12 xol-md-12">
-					<div class="section-title text-center"> 
-						
-                                        <h1>
-        <?php 
-        foreach ($blog_categories as $category) { 
-            if ($category['bc_id'] == $this->uri->segment(2)) { 
-                // Assuming the ID from the URL is in the third segment
-                echo $category['name']; 
-                break; // Stop the loop once the matching category is found
-            }
-        } 
-        ?>
-    </h1>
-                                        
-					</div>
-=======
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <!-- <div class="breadcumn-content text-left" class="wow slideInLeft" data-wow-duration="2s" data-wow-delay=".5s">
+                        <h2>All Blogs</h2>
+                        <ul>
+                            <li><a href="index.html"> <i class="fas fa-home"></i> </a></li>
+                            <li class="style2">Page</li>
+                            <li><i class="fas fa-chevron-right"></i></li>
+                            <li class="style2">All Blogs</li>
+                        </ul>
+                    </div> -->
+            </div>
+        </div>
+    </div>
 </div>
 <!-- ============================================================== -->
 <!-- Start - Service- Section -->
 <!-- ============================================================= -->
 <div class="portfolio-section single-style">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12 xol-md-12">
-				<div class="section-title text-center">
-					<h1>Blog Category Name</h1>
->>>>>>> 1a2b803147bff879ef3e4101cdc54345312222ad
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<?php foreach ($blogs as $blog) {
-				// $images = explode(',', $product['images'])
-				?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 xol-md-12">
+                <div class="section-title text-center">
+                    <h1>Blog Category Name</h1>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <?php foreach ($blogs as $blog) {
+                // $images = explode(',', $product['images'])
+                ?>
 
-				<div class="col-lg-4 col-md-6">
-					<div class="portfolio-items">
-						<div class="portfolio-thumb">
-<<<<<<< HEAD
-                                                    <img src="<?php echo base_url().'uploads/product/image/'.$blog['b_image']; ?>" alt="No image">
-=======
-							<!-- <img src="<?php echo base_url() . 'uploads/product/image/' . $blog['b_image']; ?>" alt="error"> -->
->>>>>>> 1a2b803147bff879ef3e4101cdc54345312222ad
-							<div class="thumb-content">
-								<h3>Category: <?php echo $blog['title']; ?></h3>
-								<!-- <h2>Product Name: <?php echo $blog['name']; ?></h2> -->
-								<a href="<?php echo base_url(); ?>blog_details/<?php echo $blog['b_id']; ?>">View Product <i
-										class="fas fa-long-arrow-alt-right"></i></a>
-							</div>
-						</div>
-					</div>
-				</div>
-			<?php } ?>
-		</div>
-	</div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="portfolio-items">
+                        <div class="portfolio-thumb">
+                            <!-- <img src="<?php echo base_url() . 'uploads/product/image/' . $blog['b_image']; ?>" alt="error"> -->
+                            <div class="thumb-content">
+                                <h3>Category: <?php echo $blog['title']; ?></h3>
+                                <!-- <h2>Product Name: <?php echo $blog['name']; ?></h2> -->
+                                <a href="<?php echo base_url(); ?>blog_details/<?php echo $blog['b_id']; ?>">View Product <i
+                                        class="fas fa-long-arrow-alt-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 </div>
 
 <!-- Footer start  -->
--- application/views/frontend/sectors.php
+++ application/views/frontend/sectors.php
@@ -66,7 +66,7 @@
 <!-- <<<<<<< HEAD -->
                 <!-- <img id="industry-gallery-image" src="assets/images/default.jpg" alt="" style="width:100%"> -->
                 <img id="industry-gallery-image" src="https://dummyimage.com/300x300/1e89d1/ffffff" alt="" style="width:100%">
-=======
+<!-- ======= -->
                 <!-- <img id="industry-gallery-image" src="<?php echo base_url()?>uploads/sectors/SectorImageGallary.png" alt="" style="width:100%"> -->
 <!-- >>>>>>> 1a2b803147bff879ef3e4101cdc54345312222ad -->
             </div>
