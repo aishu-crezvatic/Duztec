44d3a0c9c070205f7f4919f00de9f1fc8fd805c7
Shankar0207 <shankar.cezvatic@gmail.com>
2024-09-04 16:57:46 +0530

    changes

--- application/views/frontend/blog_category.php
+++ application/views/frontend/blog_category.php
@@ -10,11 +10,11 @@
 <!-- Start - Slider- Section -->
 <!-- ============================================================= -->
 <div class="breadcumn-section-blog d-flex align-items-center">
-<<<<<<< HEAD
-    <div class="container">
+<!--<<<<<<< HEAD-->
+<!--    <div class="container">
         <div class="row">
             <div class="col-lg-12 col-md-12">
-                <!-- <div class="breadcumn-content text-left" class="wow slideInLeft" data-wow-duration="2s" data-wow-delay=".5s">
+                 <div class="breadcumn-content text-left" class="wow slideInLeft" data-wow-duration="2s" data-wow-delay=".5s">
                         <h2>All Blogs</h2>
                         <ul>
                             <li><a href="index.html"> <i class="fas fa-home"></i> </a></li>
@@ -22,11 +22,11 @@
                             <li><i class="fas fa-chevron-right"></i></li>
                             <li class="style2">All Blogs</li>
                         </ul>
-                    </div> -->
+                    </div> 
             </div>
         </div>
-    </div>
-=======
+    </div>-->
+<!--=======-->
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12 col-md-12">
@@ -42,14 +42,14 @@
 			</div>
 		</div>
 	</div>
->>>>>>> b105e90a7770fe1eb4853df2dc075f4c56f9a544
+<!-->>>>>>> b105e90a7770fe1eb4853df2dc075f4c56f9a544-->
 </div>
 <!-- ============================================================== -->
 <!-- Start - Service- Section -->
 <!-- ============================================================= -->
 <div class="portfolio-section single-style">
-<<<<<<< HEAD
-    <div class="container">
+<!--<<<<<<< HEAD-->
+<!--    <div class="container">
         <div class="row">
             <div class="col-lg-12 xol-md-12">
                 <div class="section-title text-center">
@@ -65,10 +65,10 @@
                 <div class="col-lg-4 col-md-6">
                     <div class="portfolio-items">
                         <div class="portfolio-thumb">
-                            <!-- <img src="<?php echo base_url() . 'uploads/product/image/' . $blog['b_image']; ?>" alt="error"> -->
+                             <img src="<?php echo base_url() . 'uploads/product/image/' . $blog['b_image']; ?>" alt="error"> 
                             <div class="thumb-content">
                                 <h3>Category: <?php echo $blog['title']; ?></h3>
-                                <!-- <h2>Product Name: <?php echo $blog['name']; ?></h2> -->
+                                 <h2>Product Name: <?php echo $blog['name']; ?></h2> 
                                 <a href="<?php echo base_url(); ?>blog_details/<?php echo $blog['b_id']; ?>">View Product <i
                                         class="fas fa-long-arrow-alt-right"></i></a>
                             </div>
@@ -77,8 +77,8 @@
                 </div>
             <?php } ?>
         </div>
-    </div>
-=======
+    </div>-->
+<!--=======-->
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12 xol-md-12">
@@ -108,7 +108,7 @@
 			<?php } ?>
 		</div>
 	</div>
->>>>>>> b105e90a7770fe1eb4853df2dc075f4c56f9a544
+<!-->>>>>>> b105e90a7770fe1eb4853df2dc075f4c56f9a544-->
 </div>
 
 <!-- Footer start  -->
